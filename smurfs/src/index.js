d6a6a649e37417640cd4c0eba45a28faab09846a
Sydney Blom <sydneyblom@#gmail.com>
2019-10-11 10:30:34 -0500

    added in reducers and store

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -3,4 +3,14 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import {createStore, applyMiddleware} from "redux"
+import thunk from 'redux-thunk';
+import {Provider} from "react-redux"
+import {rootReducer} from './reducers/index';
+
+const store = createStore(rootReducer, applyMiddleware(thunk))
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById("root"));
